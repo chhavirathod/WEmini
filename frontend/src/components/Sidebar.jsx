9657a9bd226c83cb73d31dd5556827d11b482a2b
HarshvardhanPandey2003 <harshvardhanpandey2003@gmail.com>
2023-11-10 07:37:26 +0530

    Sidebar Same Error

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import navlinks from '../constants/index'
-
 import  Logo  from '../assets/logo.svg';
 import  Sun  from '../assets/sun.svg';
 
